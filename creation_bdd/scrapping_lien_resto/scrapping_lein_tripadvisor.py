66d41219b29f3c0cebcfa179918f4b979775649a
LouisBiss <80833850+LouisBiss@users.noreply.github.com>
2021-11-22 17:42:47 +0100

    Add files via upload

--- creation_bdd/scrapping_lien_resto/scrapping_lein_tripadvisor.py
+++ creation_bdd/scrapping_lien_resto/scrapping_lein_tripadvisor.py
@@ -8,15 +8,18 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
 driver = webdriver.Firefox(executable_path="C://Users//louis//Documents//COURS EMSE//Année 5 - Ingé 3//ESME//Web Scrapping//S2//geckodriver.exe")
 driver.get("https://www.tripadvisor.fr/Restaurants-g187147-Paris_Ile_de_France.html")
 
+cookies = driver.find_element_by_id('_evidon-accept-button')
+cookies.click()
 
 liste_liens_resto=[]
 
 i=0
-while i <= 0:
+while i <= 40:
     time.sleep(2)
     liste_lien_html=driver.find_elements_by_xpath('//a[@class="bHGqj Cj b"]')
     for html in liste_lien_html:
@@ -31,5 +34,14 @@ while i <= 0:
     i=i+1
 
 driver.close()
+
+#conversion liste en dataframe
+df=pd.DataFrame(liste_liens_resto,columns=['lien_resto'])
+
+#exporter le dataframe en csv
+df.to_csv('liens_resto.csv')
+
+
+
 
 
